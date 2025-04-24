e335004d71062828d3a5c30a72f5cac679442b99
Sri Hari <sesrihari@deloitte.com>
2025-04-24 16:12:35 +0530

    changes

--- src/pages/loginPage/loginForm.jsx
+++ src/pages/loginPage/loginForm.jsx
@@ -4,10 +4,12 @@ import { useLocation } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import InputField from '../../components/inputFieldComponent';
+import { ThreeDots } from 'react-loader-spinner'; // Import the loading indicator component
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false); // State to manage loading animation
   const navigate = useNavigate();
   const location = useLocation();
   const { accountCreated } = location.state || {};
@@ -21,7 +23,7 @@ const Login = () => {
   }, [navigate]);
 
   const storedUser = JSON.parse(sessionStorage.getItem('users'));
-  let matchedUser = storedUser?.find(user => user?.email === email && user?.password === password)
+  let matchedUser = storedUser?.find(user => user?.email === email && user?.password === password);
   let matchedUserEmail = storedUser?.find(user => user?.email === email);
 
   const handleSubmit = (e) => {
@@ -39,47 +41,67 @@ const Login = () => {
     }
 
     if (email && password) {
+      setLoading(true); // Show loading animation
       sessionStorage.setItem('loggedIn', true);
       alert("Login successful!");
-      navigate("/shop");
+      setTimeout(() => {
+        navigate("/shop");
+        setLoading(false); // Hide loading animation
+      }, 2000); // Simulate a delay for loading animation
     }
   };
 
-  const handlePassworChange = (e) => {
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   }
 
   return (
-    <>
-      <div> {(passwordChanged) && <p>Password changed successfully! Please log with newly changed Password</p>}</div>
-      <div className="login-container">
-        <h2 className="text-center mb-4">{(accountCreated || passwordChanged) ? "Welcome Back" : "Welcome"}</h2>
-        <form onSubmit={handleSubmit}>
-          <InputField
-            type="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value.toLowerCase());
-            }}
-            placeholder="Enter your email"
-            required
-            className="form-control"
-            label="Email"
-          />
-          <InputField
-            type="password"
-            value={password}
-            onChange={(e) => handlePassworChange(e)}
-            placeholder="Enter your password"
-            required
-            className="form-control"
-            label="Password"
-          />
-          <button type="submit" className="btn btn-primary w-100">Login</button>
-        </form>
-        <p className="mt-3 text-center">Don't have an account? <Link to="/signup" className="text-decoration-none">Sign Up</Link></p>
-        <p className="mt-3 text-center">Forgot your password ? <Link to="/forgot" className="text-decoration-none">Forgot password</Link></p>
+    <> {loading ? (
+      <div className="loading-container">
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#545a69"
+          ariaLabel="three-dots-loading"
+          wrapperClassName="landing-loader"
+          visible={true}
+          variant="bounce"
+        />
       </div>
+    ) : (
+      <div>
+        <div> {(passwordChanged) && <p>Password changed successfully! Please log with newly changed Password</p>}</div>
+        <div className="login-container">
+          <h2 className="text-center mb-4">{(accountCreated || passwordChanged) ? "Welcome Back" : "Welcome"}</h2>
+          <form onSubmit={handleSubmit}>
+            <InputField
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value.toLowerCase());
+              }}
+              placeholder="Enter your email"
+              required
+              className="form-control"
+              label="Email"
+            />
+            <InputField
+              type="password"
+              value={password}
+              onChange={(e) => handlePasswordChange(e)}
+              placeholder="Enter your password"
+              required
+              className="form-control"
+              label="Password"
+            />
+            <button type="submit" className="btn btn-primary w-100">Login</button>
+          </form>
+          <p className="mt-3 text-center">Don't have an account? <Link to="/signup" className="text-decoration-none">Sign Up</Link></p>
+          <p className="mt-3 text-center">Forgot your password ? <Link to="/forgot" className="text-decoration-none">Forgot password</Link></p>
+        </div>
+      </div>
+    )}
     </>
   );
 }
