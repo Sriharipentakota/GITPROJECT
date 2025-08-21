5349dd27e1b21c5af65923d28d687efe9ee33585
Sri Hari <sesrihari@deloitte.com>
2025-08-21 09:30:26 +0530

    new changes

--- src/resumeBuilder/components/Header.jsx
+++ src/resumeBuilder/components/Header.jsx
@@ -4,25 +4,32 @@ import { useNavigate } from 'react-router-dom'
 
 function Header() {
   const navigate = useNavigate();
+  const currentStep = localStorage.getItem('resumeCurrentStep')
   return (
     <header className="header">
       <div className="container">
         <div className="header-content" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-            <button
-              type="button"
-              onClick={() => navigate('/')}
-              aria-label="Back to Home"
-              style={{  color: '#022f77ff', border: '1px solid #5d8ad4ff', padding:'2px 4px' }}
-              className="back-btn"
-            >
-              <FiArrowLeft size={24} style={{ marginRight: 4 }} />
-            </button>
-            <div className="logo">
-              <FiFileText className="logo-icon" />
-              <span className="logo-text">ATS Resume Formatter</span>
+          {currentStep !== "edit" &&
+            <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+              <button
+                type="button"
+                onClick={() => {
+                  localStorage.removeItem('resumeData');
+                  localStorage.removeItem('resumeCurrentStep');
+                  navigate('/');
+                }}
+                aria-label="Back to Home"
+                style={{ color: '#022f77ff', border: '1px solid #5d8ad4ff', padding: '2px 4px' }}
+                className="back-btn"
+              >
+                <FiArrowLeft size={24} style={{ marginRight: 4 }} />
+              </button>
+              <div className="logo">
+                <FiFileText className="logo-icon" />
+                <span className="logo-text">ATS Resume Formatter</span>
+              </div>
             </div>
-          </div>
+          }
           <nav className="nav">
             <div className="nav-item">
               <span>Professional Resume Builder</span>
