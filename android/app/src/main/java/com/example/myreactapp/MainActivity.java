f3a752bb2a9ca5db3fdffc3469bc30a5ab91cd94
Srihari <sriharipentakota07@gmail.com>
2026-07-19 13:38:31 +0530

    QR download option

---
+++ android/app/src/main/java/com/example/myreactapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.myreactapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.myreactapp',
+  appName: 'QR-app',
+  webDir: 'build'
+};
+
+export default config;
